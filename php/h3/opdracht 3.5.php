383c1f0b8b413b586e9aa3a74b91b6af12d6978a
pepijn <23504693+pepijndik@users.noreply.github.com>
2019-02-27 10:04:04 +0100

    Add files via upload

---
+++ php/h3/opdracht 3.5.php
@@ -0,0 +1,13 @@
+<?php
+$getal1 = 5;
+$getal2 = 7;
+$getal3 = 0;
+
+$getal1 = $getal3;
+$getal2 = $getal1;
+$getal2 = $getal3;
+
+echo "De getallen worden verwisseld ... <br>"
+echo "$getal1, $getal2"
+
+?>
---
+++ php/h3/opdracht 33.php
@@ -0,0 +1,12 @@
+<?php
+$Getal1 = 50;
+$Getal2 = 60;
+if ($Getal1 > $Getal2) {
+
+    echo "Het grootste getal is $Getal1";
+} 
+else  {
+    echo "Het grootste getal is $Getal2";
+}
+
+?>
