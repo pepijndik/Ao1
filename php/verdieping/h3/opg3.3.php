545e7aa145f6754a92a1e8ce0e856009a5573e4f
pepijn <pepijn@pdik.nl>
2019-04-02 12:42:34 +0200

    Update opg3.3.php

--- php/verdieping/h3/opg3.3.php
+++ php/verdieping/h3/opg3.3.php
@@ -2,7 +2,7 @@
 <?php
 $aantal = false;
 $email = $_POST["mail"];
-for($i= 0, $i <= strlen($email); $i++;){
+for($i= 0; $i <= strlen($email); $i++){
     $var = substr($email, $i ,1);
     if($var == "@") {
         $aantal = true;
