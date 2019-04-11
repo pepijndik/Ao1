8e27df9a6d472cbd69a7284c893a926532033c0d
MSI\pepijn <pepijn@pdik.nl>
2019-04-11 14:09:46 +0200

    java explanation

--- Java/java.js
+++ Java/java.js
@@ -6,11 +6,11 @@ Start met Web development.
 //Variables outside functions are global
 
 var life = 100; // numbers
-var name = "Pdik"; //strings
-
+var name = "pdik"; //strings
+var num2 = 8;
 //rekenmachine++;  will not work because the variable is inside the function
 
-console.log("Hey! pdik"); //View text in debug (google console for example)
+console.log("Hey! " + name); //View text in debug (google console for example)
 
 function reken() {
   //Variables inside functions can only used inside the function
@@ -22,9 +22,19 @@ function reken() {
 //example 2
 function rekenen(num1, num2) {
   life = 101;
-  var rekenmachine = 0;
   console.log(num1 + num2); //where the hell comes num1 and num2 from?
 }
 
 //When calling the function you have to give 2 variables in side the /()\
-rekenen(life, rekenmachine);
+rekenen(life, num2);
+
+//objects
+
+const user = {
+  name: "pepijn",
+  age: 18,
+  girlfriend: true,
+  banks: [1, 2, 3]
+};
+
+console.log(user.girlfriend); //take a value of the object
