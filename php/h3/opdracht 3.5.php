778d79893f145401b7980cc91f7f8a7ba2d2b47c
pepijn dik <pepijn@pdik.nl>
2019-04-15 22:33:56 +0200

    commit

--- php/h3/opdracht 3.5.php
+++ php/h3/opdracht 3.5.php
@@ -7,7 +7,7 @@ $getal1 = $getal3;
 $getal2 = $getal1;
 $getal2 = $getal3;
 
-echo "De getallen worden verwisseld ... <br>"
-echo "$getal1, $getal2"
+echo ("De getallen worden verwisseld ... <br>");
+//echo ($getal1 + $getal2);
 
 ?>
