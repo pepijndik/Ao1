b73f828ab9308222017120d028671e504870ffc1
MSI\pepijn <pepijn@pdik.nl>
2019-04-26 09:38:08 +0200

    c# OPDRACHT 1

---
+++ C#/Smaltalk-jay-H2/Smaltalk-jay-H2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smaltalk_jay_H2
+{
+    public partial class Smalltalk : Form
+    {
+        public Smalltalk()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mooi weertje vandaag");
+        }
+
+        private void Smalltalk_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Maar morgen wordt regen voorspeld");
+        }
+    }
+}
