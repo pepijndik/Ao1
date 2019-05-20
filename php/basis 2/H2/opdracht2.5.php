b032869ad2043434ddd4d59325f190af675e96b6
MSI\pepijn <pepijn@pdik.nl>
2019-05-20 14:14:34 +0200

    opdracht 2.5

--- php/basis 2/H2/opdracht2.5.php
+++ php/basis 2/H2/opdracht2.5.php
@@ -1,10 +1,4 @@
 <?php
-$random = array();
-for($i = 0; $i <= 100; $i++){
-    $getal = rand(1,6) * 2;
-    
-    array_push($random,$getal);
-   
-}
-echo $random[1];
+$varArray = array();
+$value1 = $_POST["vullen"]
 ?>
