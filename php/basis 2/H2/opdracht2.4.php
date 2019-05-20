15e6ac18c7d9adf18c09cfd2ba58a6fbb2706536
MSI\pepijn <pepijn@pdik.nl>
2019-05-20 14:16:32 +0200

    Opdracht 2.4

--- php/basis 2/H2/opdracht2.4.php
+++ php/basis 2/H2/opdracht2.4.php
@@ -1,11 +1,8 @@
 <?php
-$random = array();
-for($i = 0; $i <= 100; $i++){
-    $random[$i] = rand(1,3)*2;   
+$num = array();
 $zes =0;
-for($i=1;$i<=100;$i++)
-{
-    $num[$i] = rand(1,3) * 2;
+for($i = 0; $i <= 100; $i++){
+    $num[$i] = rand(1,3)*2;   
     echo "$num[$i] <br/>"; 
     if($num[$i] == 6)
     {
