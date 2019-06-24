19922f9248e0cc7beeb5483799f7cdde97456b91
pepijn <pepijn@pdik.nl>
2019-06-24 08:49:42 +0100

    Toets commit

---
+++ php/basis 2/toets/Leeftijd Oma/oma.php
@@ -0,0 +1,28 @@
+<?PHP
+$naam1 = $_POST["naam1"];
+$naam1 = $_POST["naam2"];
+$leeftijd1 = $_POST["leeftijd1"];
+$leeftijd2 = $_POST["leeftijd2"];
+
+
+if(empty($naam1) || empty($naam2) || empty($leeftijd1) || empty($leeftijd2) ){
+echo("Vul De velden in");
+}
+else {
+uitrekenen($leeftijd1,$leeftijd2,$naam1,$naam2);
+}
+function uitrekenen($leeftijd1,$leeftijd2,$naam1, $naam2)
+   {    
+       if($leeftijd1 > 122 || $leeftijd2 > 122){
+           echo("Fout Melding!")
+       }
+       if($leeftijd1 > $leeftijd2){
+        return $naam1;
+       }
+       else if ($leeftijd1 < $leeftijd2){
+           return $naam2;
+       }
+      
+    }
+
+?>
---
+++ php/basis 2/toets/login/readme.txt
@@ -0,0 +1,9 @@
+Pepijn dik
+Toets opdracht 1 Login forum + Register. Mysql Database connection.
+
+
+Create DataBase with Mysql  (phpmyadmin).
+Toets.sql bestand uplauden.
+default login:
+username: demo  wachtwoord Demo
+username carla wachtwoord demo
